b18cd2638cd1247cc50ce1076d7e99b48b34e764
chromeoverload <chromeoverload@gmail.com>
2020-03-03 02:13:53 -0500

    start of helper methods for deleting sections and adding new tabs to sections; creating a tab on either end now auto-creates a new section

--- js/background.js
+++ js/background.js
@@ -6,6 +6,18 @@ var markers = [];
 //SETTINGS
 var spawnNewSectionAtMarker = true;
 
+function cleanSections() {
+
+}
+
+function getLeftNeighbor() {
+
+}
+
+function getRightNeighbor() {
+
+}
+
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) { //garbage collecting
     if(markers.includes(tabId)) {
         markers.splice(markers.indexOf(tabId), 1);
@@ -15,25 +27,43 @@ chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) { //garbage collec
 
 chrome.commands.onCommand.addListener(function(command) {
     if (command == 'create-divider-left') {
-        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        chrome.tabs.query({currentWindow: true, active: true }, function (tabs) {
             if(markers.includes(tabs[0].id) && spawnNewSectionAtMarker) { //if this works CONSISTENTLY, the marker tracker works!
                 chrome.tabs.create({index: (tabs[0].index), active: true, pinned: false}); //new tab
                 chrome.tabs.create({index: (tabs[0].index), url: 'index.html', active: false, pinned: false}, function(tab) {
                     markers.push(tab.id);
                 }); //left border
+            } else if (tabs[0].index === 0) {
+                chrome.tabs.create({index: (tabs[0].index), url: 'index.html', active: false, pinned: false}, function(tab) {
+                    markers.push(tab.id);
+                }); //left border
+              	chrome.tabs.create({index: (tabs[0].index), active: true, pinned: false}); //new tab
             } else {
                 chrome.tabs.create({index: (tabs[0].index), url: 'index.html', active: false, pinned: false}, function(tab) {
                     markers.push(tab.id);
                 });
             }
         });
-    } else { //create-divider-right
-        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    } else if (command == 'delete-section') {
+        chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+
+        });
+    } else {
+        var numTabs = -1;
+        chrome.tabs.query({currentWindow: true}, function (tabs) {
+            numTabs += Object.keys(tabs).length;
+        });
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs) { //create-divider-right
             if(markers.includes(tabs[0].id) && spawnNewSectionAtMarker) { //if this works CONSISTENTLY, the marker tracker works!
                 chrome.tabs.create({index: (tabs[0].index + 1), url: 'index.html', active: false, pinned: false}, function(tab) {
                     markers.push(tab.id);
                 }); //right border
                 chrome.tabs.create({index: (tabs[0].index + 1), active: true, pinned: false}); //new tab
+            } else if (tabs[0].index == (numTabs)) {
+              	chrome.tabs.create({index: (tabs[0].index + 1), active: true, pinned: false}); //new tab
+                chrome.tabs.create({index: (tabs[0].index + 1), url: 'index.html', active: false, pinned: false}, function(tab) {
+                    markers.push(tab.id);
+                }); //left border
             } else {
                 chrome.tabs.create({index: (tabs[0].index + 1), url: 'index.html', active:false, pinned: false}, function(tab) {
                     markers.push(tab.id);
