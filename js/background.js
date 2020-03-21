eb62446e7240f617a58d29e86151dc8a2ad700fc
chromeoverload <chromeoverload@gmail.com>
2020-03-21 01:27:22 -0500

    delete sections function (in progress), addition of chrome-extension-async library (unused)

--- js/background.js
+++ js/background.js
@@ -1,21 +1,29 @@
 // Copyleft 2020 Jared Butler
 
 var markers = [];
-//var sections = [];
 
 //SETTINGS
 var spawnNewSectionAtMarker = true;
+//import * as chromeAsync from './node-modules/chrome-extension-async/chrome-extension-async.js';
 
-function cleanSections() {
+function getBounds(tabs) {
+    var bounds = [0, Object.keys(tabs).length - 1]
+    var foundTab = false;
 
-}
+    for(var i = 0; i < tabs.length; i++) {
+        if(!foundTab) {
+            foundTab = tabs[i].active;
+            if(markers.includes(tabs[i].id)) {
+                bounds[0] = i;
+            }
+        }
+        if(foundTab && markers.includes(tabs[i].id)) {
+            bounds[1] = i;
+            break;
+        }
+    }
 
-function getLeftNeighbor() {
-
-}
-
-function getRightNeighbor() {
-
+    return bounds;
 }
 
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) { //garbage collecting
@@ -23,6 +31,14 @@ chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) { //garbage collec
         markers.splice(markers.indexOf(tabId), 1);
         //console.log(tabId);
     }
+});
+
+chrome.tabs.onCreated.addListener(function(tab) { //new tabs added to relevant section
+    chrome.tabs.query({currentWindow: true}, function (tabs) {
+        var bounds = getBounds(tabs);
+
+        //chrome.tabs.move(tab.id, { index: tabs[bounds[1] - 1].index});
+    });
 });
 
 chrome.commands.onCommand.addListener(function(command) {
@@ -37,7 +53,7 @@ chrome.commands.onCommand.addListener(function(command) {
                 chrome.tabs.create({index: (tabs[0].index), url: 'index.html', active: false, pinned: false}, function(tab) {
                     markers.push(tab.id);
                 }); //left border
-              	chrome.tabs.create({index: (tabs[0].index), active: true, pinned: false}); //new tab
+                chrome.tabs.create({index: (tabs[0].index), active: true, pinned: false}); //new tab
             } else {
                 chrome.tabs.create({index: (tabs[0].index), url: 'index.html', active: false, pinned: false}, function(tab) {
                     markers.push(tab.id);
@@ -45,14 +61,20 @@ chrome.commands.onCommand.addListener(function(command) {
             }
         });
     } else if (command == 'delete-section') {
-        chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+        chrome.tabs.query({currentWindow: true}, function(tabs) {
+            var bounds = getBounds(tabs);
 
+            for(var i = bounds[0]; i <= bounds[1]; i++) {
+                chrome.tabs.remove(tabs[i].id);
+            }
         });
-    } else {
+    } else { //create-divider-right
+        //TAB LENGTH LOGIC (necessary??)
         var numTabs = -1;
         chrome.tabs.query({currentWindow: true}, function (tabs) {
             numTabs += Object.keys(tabs).length;
         });
+
         chrome.tabs.query({currentWindow: true, active: true}, function (tabs) { //create-divider-right
             if(markers.includes(tabs[0].id) && spawnNewSectionAtMarker) { //if this works CONSISTENTLY, the marker tracker works!
                 chrome.tabs.create({index: (tabs[0].index + 1), url: 'index.html', active: false, pinned: false}, function(tab) {
